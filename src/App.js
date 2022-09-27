5d3e83994b5bc6742bc93211b394374dbc8da978
Trần Đức Bo <94088465+baotran123456@users.noreply.github.com>
2022-09-28 05:31:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Navbar from './Screen/Navbar';
 import Home from './Screen/Home';
 import Introduce from './Screen/Introduce';
 import Product from './Screen/Product';
-import Personal from './Screen/Personal';
+import Detail from './Screen/Detail';
 import './Screen/Introduce.css'
 import './Screen/Home.css'
 import './Screen/Product.css'
-
+import './Screen/Detail.css'
 
 function App() {
   return (
@@ -24,7 +24,7 @@ function App() {
         <Route path='/Home' element={<Home />} />
         <Route path='/Introduce' element={<Introduce />} />
         <Route path='/Product' element={<Product />} />
-        <Route path='/Personal' element={<Personal />} />
+        <Route path='/Detail' element={<Detail />} />
       </Routes>
     </div>
   );
