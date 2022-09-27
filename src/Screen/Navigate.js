a3b7a4c47b0d92032f28fcd8320a316ead8e53f2
Trần Đức Bo <94088465+baotran123456@users.noreply.github.com>
2022-09-27 22:53:10 +0700

    bao

--- src/Screen/Navigate.js
+++ src/Screen/Navigate.js
@@ -1,19 +1,147 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import '../Screen/Navbar.css'
-import Home from "./Home";
-import Introduce from "./Introduce";
-import Navbar from "./Navbar";
+import { NavLink } from "react-router-dom";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import AccountCircleSharpIcon from '@mui/icons-material/AccountCircleSharp';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './Navigate.css'
 
-
+const logo = "https://scontent.xx.fbcdn.net/v/t1.15752-9/305305021_5469725353149061_8412010419326309420_n.png?stp=dst-png_p228x119&_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_ohc=7Zi8f3uJ7LcAX-Ahk9V&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVKEfUaHtXLeMZOTR6YHO_vyTHkbIJMJf_X2Mc2tGwmG7g&oe=6359FA2E";
+const image5 = "https://thuytinhtadaco.com/wp-content/uploads/2021/08/chai-lo-thuy-tinh.jpg";
 function Navigate() {
+    const uudai = [
+        {
+            id: 1,
+            image: image5,
+            name: 'Top 6 nóm đồ 2hand không nên mua',
+            mota: 'Hệ Cao đẳng thực hành thuộc Đại học FPT, còn gọi là FPT Polytechnic, được thực hiện đào tạo và cấp bằng Cao đẳng nghề theo Quyết định của Tổng cục dạy nghề bắt đầu từ ngày 01/07/2010. Trường hướng tới mục tiêu cung cấp kỹ năng nghề nghiệp chất lượng cao cho người học, đảm bảo được tính cạnh tranh trong môi trường phát triển ngày càng cao.',
+        },
+        {
+            id: 1,
+            image: image5,
+            name: 'Top 6 nóm đồ 2hand không nên mua',
+            mota: 'Hệ Cao đẳng thực hành thuộc Đại học FPT, còn gọi là FPT Polytechnic, được thực hiện đào tạo và cấp bằng Cao đẳng nghề theo Quyết định của Tổng cục dạy nghề bắt đầu từ ngày 01/07/2010. Trường hướng tới mục tiêu cung cấp kỹ năng nghề nghiệp chất lượng cao cho người học, đảm bảo được tính cạnh tranh trong môi trường phát triển ngày càng cao.',
+        },
+        {
+            id: 1,
+            image: image5,
+            name: 'Top 6 nóm đồ 2hand không nên mua',
+            mota: 'Hệ Cao đẳng thực hành thuộc Đại học FPT, còn gọi là FPT Polytechnic, được thực hiện đào tạo và cấp bằng Cao đẳng nghề theo Quyết định của Tổng cục dạy nghề bắt đầu từ ngày 01/07/2010. Trường hướng tới mục tiêu cung cấp kỹ năng nghề nghiệp chất lượng cao cho người học, đảm bảo được tính cạnh tranh trong môi trường phát triển ngày càng cao.',
+        },
+        {
+            id: 1,
+            image: image5,
+            name: 'Top 6 nóm đồ 2hand không nên mua',
+            mota: 'Hệ Cao đẳng thực hành thuộc Đại học FPT, còn gọi là FPT Polytechnic, được thực hiện đào tạo và cấp bằng Cao đẳng nghề theo Quyết định của Tổng cục dạy nghề bắt đầu từ ngày 01/07/2010. Trường hướng tới mục tiêu cung cấp kỹ năng nghề nghiệp chất lượng cao cho người học, đảm bảo được tính cạnh tranh trong môi trường phát triển ngày càng cao.',
+        }
+    ]
     return (
-        <div>
-            <Navbar/>
-            <Routes>
-                <Route path="/Home" element={<Home />}/>
-                <Route path="/Introduce" element={<Introduce />}/>
-            </Routes>
+        <div className="home">
+            <nav className="home-header">
+                <h3 style={{ width: "20%", textAlign: "center" }}>logo</h3>
+                <ul className="home-header_ul">
+                    <li>
+                        <NavLink className="home-header_ul_li_navlink" to="/Home">Trang chủ</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="home-header_ul_li_navlink" to="/Introduce">Giới thiệu</NavLink>
+                    </li>
+                    <li className="home-header_ul_subnav">
+                        <NavLink className="home-header_ul_li_navlink" to="/Product">Sản phẩm</NavLink>
+                        <ArrowDropDownIcon className="home-header_ul_li_navlink_icon" />
+                        <div className="home-header_ul_subnav_content">
+                            <a href="#company">Company</a>
+                            <a href="#team">Team</a>
+                            <a href="#careers">Careers</a>
+                        </div>
+                    </li>
+                    <li>
+                        <NavLink className="home-header_ul_li_navlink" to="/Navigate">Ưu đãi</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="home-header_ul_li_navlink" to="/Introduce">Liên hệ</NavLink>
+                    </li>
+                </ul>
+                <div className="home-header_icon">
+                    <SearchSharpIcon />
+
+                    <div className="home-header_icon_user">
+                        <AccountCircleSharpIcon className="home-header_icon_user_img" />
+                        <div className="home-header_icon_user_content">
+                            <a href="#company">Company</a>
+                            <a href="#team">Team</a>
+                            <a href="#careers">Careers</a>
+                        </div>
+                    </div>
+
+                    <ShoppingCartIcon />
+                </div>
+            </nav>
+            <div className="title">
+                <div className="title_header">
+                    <h2>Tin tức & Ưu đãi</h2>
+                </div>
+                <div className="title_list">
+                    <div >
+                        {uudai.map(uudai =>
+                            <div className="title_list_cottom">
+                                <img src={uudai.image}/>
+                                <p>{uudai.mota}</p>
+                                <button>Đọc Thêm</button>
+                            </div>)}
+                    </div>
+
+                </div>
+            </div>
+            <div className="hashtag">
+                <div className="hashtag_header">
+                    <div className="hashtag_header--list">
+                        <p>Hashtag để có cơ hội xuất hiện trên trang chúng tôi</p>
+                        <img src={image5}></img>
+                        <img src={image5}></img>
+                        <img src={image5}></img>
+                        <img src={image5}></img>
+                        <img src={image5}></img>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <div className="footer_container">
+                    <img src={logo} />
+                    <br />
+                    <input></input>
+                    <br />
+                    <div className="footer_container--page" >
+                        <div className="footer_container--page--page">
+                            <h2>FANPAGE</h2>
+                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=331&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="331" ></iframe>
+                        </div>
+                        <div className="footer_container--page--company" >
+                            <h2>Công ty TNHH SẢN XUẤT ĐẦU TƯ
+                                TM DV XNK VŨ GIA GROUP</h2>
+                            <p>840/20B Hương Lộ 2, Phường Bình Trị Đông A,
+                                Quận Bình Tân, Thành phố Hồ Chí Minh, Việt Nam</p>
+                            <p>(84+) 706 185 986</p>
+                            <p>vagabonds.2hand@gmail.comt</p>
+                        </div>
+                        <div className="footer_container--page--we" >
+                            <h2>Về chúng tôi</h2><br></br>
+                            <p>Giới thiệu</p>
+                            <p>Sản phẩm</p>
+                            <p>Ưu đãi</p>
+                            <p>Liên hệ</p>
+                        </div>
+                        <div className="footer_container--page--policy" >
+                            <h2>Chính sách</h2><br />
+                            <p>Chính sách bảo mật</p>
+                            <p>Chính sách đổi trả</p>
+                            <p>Chính sách bảo hành </p>
+                            <p>Chính sách giao hàng</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
