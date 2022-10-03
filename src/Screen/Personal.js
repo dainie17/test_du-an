8de7f4f2dcafca8604624918c28b22f9dec488dc
Trần Đức Bo <94088465+baotran123456@users.noreply.github.com>
2022-10-03 18:58:03 +0700

    chodai

--- src/Screen/Personal.js
+++ src/Screen/Personal.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import AccountCircleSharpIcon from '@mui/icons-material/AccountCircleSharp';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import './Personal.css'
+import { pink } from "@mui/material/colors";
 
 const logo = "https://scontent.xx.fbcdn.net/v/t1.15752-9/305305021_5469725353149061_8412010419326309420_n.png?stp=dst-png_p228x119&_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_ohc=7Zi8f3uJ7LcAX-Ahk9V&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVKEfUaHtXLeMZOTR6YHO_vyTHkbIJMJf_X2Mc2tGwmG7g&oe=6359FA2E";
 
 
 function Person() {
+    const [toggleState, setToggleState] = useState(1);
+    const toogleTab = (index) => {
+        setToggleState(index);
+    }
     return (
         <div className="fersonal">
-             <nav className="home-header">
+            <nav className="home-header">
                 <h3 style={{ width: "20%", textAlign: "center" }}>logo</h3>
                 <ul className="home-header_ul">
                     <li>
@@ -57,12 +63,12 @@ function Person() {
                 <div className="fersonal_container-left">
                     <h2>Tài khoản của tôi</h2>
                     <div className="fersonal_container-left--info">
-                        <a href="#1">Thông tin</a>
-                        <a href="#2">Thông tin</a>
-                        <a href="#3">Thông tin</a>
-                        <a href="#4">Thông tin</a>
-                        <a href="#5">Thông tin</a>
-                        <a href="#6">Thông tin</a>
+                        <div className="fersonal_container-left--info--tabs" onClick={() => toogleTab(1)}>Thông tin</div>
+                        <div className="fersonal_container-left--info--tabs" onClick={() => toogleTab(2)}>Sản phẩm ưa thích</div>
+                        <div className="fersonal_container-left--info--tabs" onClick={() => toogleTab(3)}>Giỏ hàng</div>
+                        <div className="fersonal_container-left--info--tabs" onClick={() => toogleTab(4)}>Đơn đặt hàng của tôi</div>
+                        <div className="fersonal_container-left--info--tabs" onClick={() => toogleTab(5)}>Tình trạng đơn hàng</div>
+                        <div className="fersonal_container-left--info--tabs" onClick={() => toogleTab(6)}>Đăng xuất</div>
                     </div>
                 </div>
                 <div className="fersonal_container-right">
@@ -71,24 +77,41 @@ function Person() {
 
                         </div>
                         <div className="fersonal_container-right--header--content">
-                            <div className="content">
-                            <AccountCircleSharpIcon sx={{fontSize:50}}/>
-                            <p>Đơn hàng của tôi</p>
+                            <div className="content" onClick={() => toogleTab(4)}>
+                                <AccountCircleSharpIcon sx={{ fontSize: 50, color: '#ffa726' }} />
+                                <p>Đơn hàng của tôi</p>
                             </div>
-                            <div className="content">
-                            <LocalTaxiIcon sx={{fontSize:50}}/>
-                            <p>Tình trạng đơn hàng</p>
+                            <div className="content" onClick={() => toogleTab(5)}>
+                                <LocalTaxiIcon sx={{ fontSize: 50 , color: '#ffa726'}} />
+                                <p>Tình trạng đơn hàng</p>
                             </div>
-                            <div className="content">
-                            <AccountCircleSharpIcon sx={{fontSize:50}}/>
-                            <p>Yêu thích</p>
+                            <div className="content" onClick={() => toogleTab(1)}>
+                                <FavoriteIcon sx={{ fontSize: 50, color: pink[500] }} />
+                                <p>Yêu thích</p>
                             </div>
                         </div>
-                        
-                        
+
+
                     </div>
                     <div className="fersonal_container-right--fooder--content">
-
+                        <div className={toggleState === 1 ? "contentt active-content" : "ac"}>
+                            <p>Thông tin</p>
+                        </div>
+                        <div className={toggleState === 2 ? "contentt active-content" : "ac"}>
+                            <p>Sản phẩm ưa thích</p>
+                        </div>
+                        <div className={toggleState === 3 ? "contentt active-content" : "ac"}>
+                            <p>Giỏ Hàng</p>
+                        </div>
+                        <div className={toggleState === 4 ? "contentt active-content" : "ac"}>
+                            <p>Đơn hàng</p>
+                        </div>
+                        <div className={toggleState === 5 ? "contentt active-content" : "ac"}>
+                            <p>Tình trạng</p>
+                        </div>
+                        <div className={toggleState === 6 ? "contentt active-content" : "ac"}>
+                            <p>Đăng xuất</p>
+                        </div>
                     </div>
                 </div>
             </div>
