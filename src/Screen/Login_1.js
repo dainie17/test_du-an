2b84882cac1c6c5414db0f893caadf7372d3682b
dainie17 <daiabxc@gmail.com>
2022-10-06 03:02:43 +0700

    login pass 8

--- src/Screen/Login_1.js
+++ src/Screen/Login_1.js
@@ -1,129 +1,132 @@
-import React from 'react';
-import * as Yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import RHFTextField from '../bo_sung/RHFTextField';
-import { Stack, IconButton, InputAdornment, Checkbox } from '@mui/material';
-import { useForm } from 'react-hook-form';
-import FormProvider from '../bo_sung/FormProvider';
-import '../css/Login_1.css'
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import React from "react";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import RHFTextField from "../bo_sung/RHFTextField";
+import { Stack, IconButton, InputAdornment, Checkbox } from "@mui/material";
+import { useForm } from "react-hook-form";
+import FormProvider from "../bo_sung/FormProvider";
+import { LoadingButton } from "@mui/lab";
+import "../css/Login_1.css";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 function Login_1() {
-    const navigate = useNavigate();
-    const [showPassConf, setShowPassConf] = useState(false);
+  const navigate = useNavigate();
 
-    const LoginSchema = Yup.object().shape({
-        email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-        password: Yup.string().required('Password is required'),
-    });
+  const [showPassConf, setShowPassConf] = useState(false);
 
-    const defaultValues = {
-        email: '',
-        password: '',
-        remember: true,
-    };
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Email must be a valid email address")
+      .required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
 
-    const methods = useForm({
-        resolver: yupResolver(LoginSchema),
-        defaultValues,
-    });
+  const defaultValues = {
+    email: "",
+    password: "",
+    remember: true,
+  };
 
-    const {
-        handleSubmit,
-        formState: { isSubmitting },
-    } = methods;
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
 
-    const onSubmit = async () => {
-        navigate('/Home', { replace: true });
-    };
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
 
-    return (
-        <div className="container">
-            <div className="logo_form">
-                <img className='logo' src='https://pngimg.com/uploads/bmw_logo/bmw_logo_PNG19705.png' />
-                <h1 className='name_logo'>Vagaboon</h1>
-            </div>
+  const onSubmit = async () => {
+    navigate("/Home", { replace: true });
+  };
 
-            <div className='login'>
-                <div className='form_login_title'>
-                    <p className='title_content'>Đăng nhập</p>
-                    <p className='nouser'>Không có tài khoản ?</p>
-                </div>
-                <div className='form_login_input'>
-                    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-                        <Stack spacing={3}>
-                            <RHFTextField name="email" label="Email address" />
+  return (
+    <div className="container">
+      <div className="logo_form">
+        <img
+          className="logo"
+          src="https://pngimg.com/uploads/bmw_logo/bmw_logo_PNG19705.png"
+        />
+        <h1 className="name_logo">Vagaboon</h1>
+      </div>
 
-                            <RHFTextField
-                                name="password"
-                                label="Password"
-                                type={showPassConf ? 'text' : 'password'}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton onClick={() => setShowPassConf(!showPassConf)}>
-                                                {showPassConf ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Stack>
-                    </FormProvider>
-                </div>
-
-                <div className='form_login_forgetmenot'>
-                    <div className='form_login_forgetmenot_left'>
-                        <Checkbox />
-                        <div className='title_chb'>Lưu mật khẩu</div>
-                    </div>
-                    <p>Quên mật khẩu ?</p>
-                </div>
-
-                <div className='form_login_btn'>
-                    Đăng nhập
-                </div>
-                <div className='login_or'>Hoặc</div>
-                <div className='form_login_with'>
-
-                    <div className='btn_gg'>
-                        <div className='logo_gg'></div>
-                        <div className='title_gg'>
-                            Google
-                        </div>
-                    </div>
-
-                    <div className='btn_tw'>
-                        <div  className='logo_tw'/>
-                        <div className='title_tw'>
-                            Twiter
-                        </div>
-                    </div>
-
-                    <div className='btn_fb'>
-                        <div className='logo_fb'></div>
-                        <div className='title_fb'>
-                            Facebook
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            <div className='footer_login'>
-                <div className='footer_login_content'>
-                    <div className='about_we'>Về chúng tôi</div>
-                    <div className='about_line'></div>
-                    <div className='about_we'>Các điều khoản</div>
-                    <div className='about_line'></div>
-                    <div className='about_we'>Chính sách bảo mật</div>
-                    <div className='about_line'></div>
-                    <div className='about_we'>Trung tâm trợ giúp</div>
-                </div>
-                <div className=''>©2022 Bản quyền thuộc về Công Ty TNHH Sản Xuất Đầu Tư TM DV XNK Vũ Gia Group.</div>
-            </div>
+      <div className="login">
+        <div className="form_login_title">
+          <p className="title_content">Đăng nhập</p>
+          <p className="nouser">Không có tài khoản ?</p>
         </div>
-    )
+        <div className="form_login_input">
+          <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={3}>
+              <RHFTextField name="email" label="Email address" />
+
+              <RHFTextField
+                name="password"
+                label="Password"
+                type={showPassConf ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setShowPassConf(!showPassConf)}
+                      >
+                        {showPassConf ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+            <div className="form_login_forgetmenot">
+              <div className="form_login_forgetmenot_left">
+                <Checkbox />
+                <div className="title_chb">Lưu mật khẩu</div>
+              </div>
+              <p>Quên mật khẩu ?</p>
+            </div>
+            <button className='form_login_btn' type="submit" loading={isSubmitting}>
+                    Đăng nhập
+                </button>
+          </FormProvider>
+        </div>
+
+        <div className="login_or">Hoặc</div>
+        <div className="form_login_with">
+          <div className="btn_gg">
+            <div className="logo_gg"></div>
+            <div className="title_gg">Google</div>
+          </div>
+
+          <div className="btn_tw">
+            <div className="logo_tw" />
+            <div className="title_tw">Twiter</div>
+          </div>
+
+          <div className="btn_fb">
+            <div className="logo_fb"></div>
+            <div className="title_fb">Facebook</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer_login">
+        <div className="footer_login_content">
+          <div className="about_we">Về chúng tôi</div>
+          <div className="about_line"></div>
+          <div className="about_we">Các điều khoản</div>
+          <div className="about_line"></div>
+          <div className="about_we">Chính sách bảo mật</div>
+          <div className="about_line"></div>
+          <div className="about_we">Trung tâm trợ giúp</div>
+        </div>
+        <div className="">
+          ©2022 Bản quyền thuộc về Công Ty TNHH Sản Xuất Đầu Tư TM DV XNK Vũ Gia
+          Group.
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Login_1;
