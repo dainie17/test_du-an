ea6827fd9a2dd044b433842306a5201bf97aa2ad
Trần Đức Bo <94088465+baotran123456@users.noreply.github.com>
2022-10-11 21:37:36 +0700

    thongtin

--- src/Screen/Personal.js
+++ src/Screen/Personal.js
@@ -17,12 +17,46 @@ import AutorenewIcon from '@mui/icons-material/Autorenew';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
 import { DeleteOutlineSharp, FavoriteBorderSharp, VerifiedUserSharp } from "@mui/icons-material";
 import '../css/Personal.css'
 import { pink } from "@mui/material/colors";
 
 const logo = "https://scontent.xx.fbcdn.net/v/t1.15752-9/305305021_5469725353149061_8412010419326309420_n.png?stp=dst-png_p228x119&_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_ohc=7Zi8f3uJ7LcAX-Ahk9V&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVKEfUaHtXLeMZOTR6YHO_vyTHkbIJMJf_X2Mc2tGwmG7g&oe=6359FA2E";
-const img ="https://znews-photo.zingcdn.me/w660/Uploaded/qhj_yvobvhfwbv/2018_07_18/Nguyen_Huy_Binh1.jpg";
+const img = "https://znews-photo.zingcdn.me/w660/Uploaded/qhj_yvobvhfwbv/2018_07_18/Nguyen_Huy_Binh1.jpg";
+
+// online
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}));
 
 function Person() {
     const [toggleState, setToggleState] = useState(1);
@@ -120,6 +154,15 @@ function Person() {
                 <div className="fersonal_container-right">
                     <div className="fersonal_container-right--header">
                         <div className="fersonal_container-right--header--avatar">
+                            <div className="fersonal_container-right--header--avatar--user">
+                                <Avatar className="fersonal_container-right--header--avatar--user--icon1" src={img} sx={{ width: 100, height: 100, zIndex: 3 }} />
+
+                            </div>
+
+
+                            <div className="fersonal_container-right--header--avatar--name">
+                                <h3>Đặng Quang Hùng</h3>
+                            </div>
                         </div>
                         <div className="fersonal_container-right--header--content">
                             <div className="content" onClick={() => toogleTab(4)}>
@@ -140,7 +183,55 @@ function Person() {
                     </div>
                     <div className="fersonal_container-right--fooder--content">
                         <div className={toggleState === 1 ? "contentt active-content" : "ac"}>
-                            <p>Thong Tin</p></div>
+                            <div className="fersonal_container-right--fooder--content--active">
+                                <div>
+                                <StyledBadge
+                                    overlap="circular"
+                                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                                    variant="dot"
+                                >
+                                    <Avatar alt="Remy Sharp" src={img} />
+                                    
+                                </StyledBadge>
+                                <p>Dang Quang Hung</p>
+                                <button className="fersonal_container-right--fooder--content--active--button">Chinh sau Thong tin</button>
+                                </div>
+                                <div className="fersonal_container-right--fooder--content--active--edit">
+                                    <div className="fersonal_container-right--fooder--content--active--edit--name">
+                                        <div className="fersonal_container-right--fooder--content--active--edit--name--p">
+                                            <p className="name--login">Tên Đăng Nhập</p>
+                                            <p className="name--people">Đặng Quang Hùng</p>
+                                        </div>
+                                        <div className="fersonal_container-right--fooder--content--active--edit--name--button">
+                                            <button>Chỉnh Sửa</button>
+                                        </div>
+                                    </div>
+                                    <div className="fersonal_container-right--fooder--content--active--edit--name">
+                                        <div className="fersonal_container-right--fooder--content--active--edit--name--p">
+                                            <p className="name--login">Email</p>
+                                            <p className="name--people">**********@gmail.com <a href="#1">Hiển thị</a></p>
+                                        </div>
+                                        <div className="fersonal_container-right--fooder--content--active--edit--name--button">
+                                            <button>Chỉnh Sửa</button>
+                                        </div>
+                                    </div>
+                                    <div className="fersonal_container-right--fooder--content--active--edit--name">
+                                        <div className="fersonal_container-right--fooder--content--active--edit--name--p">
+                                            <p className="name--login">Số điện thoại</p>
+                                            <p className="name--people">Bạn chưa thêm số điện thoại nào cả</p>
+                                        </div>
+                                        <div className="fersonal_container-right--fooder--content--active--edit--name--button">
+                                            <button>Thêm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="fersonal_container-right--fooder--content--active--password">
+                                    <h3>Mật Khẩu</h3>
+                                    <button>Đổi mật khẩu</button>
+                                    
+                                </div>
+                            </div>
+                        </div>
                         <div className={toggleState === 2 ? "contentt active-content" : "ac"}>
                             <p>Sản phẩm ưa thích</p>
                         </div>
@@ -152,68 +243,68 @@ function Person() {
                                 {
                                     user.map((item, index) => (
                                         <div>
-                                        <div key={index} className="active-content-list-card">
-                                            <img className="active-content-list-card-image" src={item.image} alt="" />
-                                            <div className="active-content-list-card-content">
-                                                <p className="active-content-list-card-content-name">{item.name}</p>
-                                                <p className="active-content-list-card-content-type">Loại:{item.Loai}/Ship từ:{item.Ship}</p>
-                                                <p className="active-content-list-card-content-price">{item.price}&#8363;</p>
-                                                <p className="active-content-list-card-content-cod"><p>COD</p></p>
-                                            </div>
-                                            <div className="active-content-list-card-function">
-                                                <div className="active-content-list-card-function-button">
-                                                    <button className="active-content-list-card-function-button-reduce">-</button>
-                                                    <p className="active-content-list-card-function-button-num">1</p>
-                                                    <button className="active-content-list-card-function-button-more">+</button>
+                                            <div key={index} className="active-content-list-card">
+                                                <img className="active-content-list-card-image" src={item.image} alt="" />
+                                                <div className="active-content-list-card-content">
+                                                    <p className="active-content-list-card-content-name">{item.name}</p>
+                                                    <p className="active-content-list-card-content-type">Loại:{item.Loai}/Ship từ:{item.Ship}</p>
+                                                    <p className="active-content-list-card-content-price">{item.price}&#8363;</p>
+                                                    <p className="active-content-list-card-content-cod"><p>COD</p></p>
                                                 </div>
-                                                <div className="active-content-list-card-function-icon">
-                                                    <FavoriteBorderSharp />
-                                                    <DeleteOutlineSharp />
+                                                <div className="active-content-list-card-function">
+                                                    <div className="active-content-list-card-function-button">
+                                                        <button className="active-content-list-card-function-button-reduce">-</button>
+                                                        <p className="active-content-list-card-function-button-num">1</p>
+                                                        <button className="active-content-list-card-function-button-more">+</button>
+                                                    </div>
+                                                    <div className="active-content-list-card-function-icon">
+                                                        <FavoriteBorderSharp />
+                                                        <DeleteOutlineSharp />
+                                                    </div>
                                                 </div>
+
                                             </div>
-                                           
-                                        </div>
-                                        <div className="personal-br"></div>
+                                            <div className="personal-br"></div>
                                         </div>
                                     ))
                                 }
-                                
+
                             </div>
                         </div>
                         <div className={toggleState === 4 ? "contentt active-content" : "ac"}>
-                        <div className="active-content-title">
+                            <div className="active-content-title">
                                 <p className="active-content-title-content">Giỏ hàng </p>
                             </div>
                             <div className="active-content-list">
                                 {
                                     user.map((item, index) => (
                                         <div>
-                                        <div key={index} className="active-content-list-card">
-                                            <img className="active-content-list-card-image" src={item.image} alt="" />
-                                            <div className="active-content-list-card-content">
-                                                <p className="active-content-list-card-content-name">{item.name}</p>
-                                                <p className="active-content-list-card-content-type">Loại:{item.Loai}/Ship từ:{item.Ship}</p>
-                                                <p className="active-content-list-card-content-price">{item.price}&#8363;</p>
-                                                <p className="active-content-list-card-content-cod"><p>COD</p></p>
-                                            </div>
-                                            <div className="active-content-list-card-function">
-                                                <div className="active-content-list-card-function-button">
-                                                    <button className="active-content-list-card-function-button-reduce">-</button>
-                                                    <p className="active-content-list-card-function-button-num">1</p>
-                                                    <button className="active-content-list-card-function-button-more">+</button>
+                                            <div key={index} className="active-content-list-card">
+                                                <img className="active-content-list-card-image" src={item.image} alt="" />
+                                                <div className="active-content-list-card-content">
+                                                    <p className="active-content-list-card-content-name">{item.name}</p>
+                                                    <p className="active-content-list-card-content-type">Loại:{item.Loai}/Ship từ:{item.Ship}</p>
+                                                    <p className="active-content-list-card-content-price">{item.price}&#8363;</p>
+                                                    <p className="active-content-list-card-content-cod"><p>COD</p></p>
                                                 </div>
-                                                <div className="active-content-list-card-function-icon">
-                                                    <FavoriteBorderSharp />
-                                                    <DeleteOutlineSharp />
+                                                <div className="active-content-list-card-function">
+                                                    <div className="active-content-list-card-function-button">
+                                                        <button className="active-content-list-card-function-button-reduce">-</button>
+                                                        <p className="active-content-list-card-function-button-num">1</p>
+                                                        <button className="active-content-list-card-function-button-more">+</button>
+                                                    </div>
+                                                    <div className="active-content-list-card-function-icon">
+                                                        <FavoriteBorderSharp />
+                                                        <DeleteOutlineSharp />
+                                                    </div>
                                                 </div>
+
                                             </div>
-                                           
-                                        </div>
-                                        <div className="personal-br"></div>
+                                            <div className="personal-br"></div>
                                         </div>
                                     ))
                                 }
-                                
+
                             </div>
                         </div>
                         <div className={toggleState === 5 ? "contentt active-content" : "ac"}>
